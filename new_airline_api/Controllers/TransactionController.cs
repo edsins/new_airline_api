5354d974f9996ab79a35af676abead557ae2331d
edsins <adityasingh_16738@aitpune.edu.in>
2021-02-04 10:46:48 +0530

    changed email to contact_email

--- new_airline_api/Controllers/TransactionController.cs
+++ new_airline_api/Controllers/TransactionController.cs
@@ -23,7 +23,7 @@ namespace new_airline_api.Controllers
                 return BadRequest(ModelState);
             }
 
-            var user = db.User_Master.Where(x => x.email_id == tp.email).FirstOrDefault();
+            var user = db.User_Master.Where(x => x.email_id == tp.contact_email).FirstOrDefault();
             if(user==null)
             {
                 return BadRequest("User does not Exists");
