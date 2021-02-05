a65e271cebea9517a0dfb7b346ba260181b99a4b
edsins <adityasingh_16738@aitpune.edu.in>
2021-02-05 09:37:01 +0530

    added adminlogin

---
+++ new_airline_api/Controllers/AdminLoginController.cs
@@ -0,0 +1,38 @@
+﻿using new_airline_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace new_airline_api.Controllers
+{
+    public class AdminLoginController : ApiController
+    {
+        private new_airlineEntities db = new new_airlineEntities();
+        [HttpPost]
+        public IHttpActionResult adminloginvalid(userlogin u)
+        {
+            var admin = db.admins.Where(x => x.username == u.email).FirstOrDefault();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (admin != null)
+            {
+                bool isValid = BCrypt.Net.BCrypt.Verify(u.password, admin.password);
+                if (!isValid)
+                {
+                    return BadRequest("Invalid Credentials");
+                }
+
+            }
+            else
+            {
+                return BadRequest("Invalid Credentials");
+            }
+            return Ok("Valid");
+        }
+    }
+}
