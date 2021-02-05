66954e1afdb34676adf6691605b79711a08eb849
Anurag <anuragagrawal4998@gmail.com>
2021-02-05 09:02:37 +0530

    add a class for validating otp

---
+++ new_airline_api/Models/validateotp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace new_airline_api.Models
+{
+    public class validateotp
+    {
+        public string email { get; set; }
+        public int otp { get; set; }
+    }
+}
